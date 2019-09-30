34dc5c11f7ba93b65ed376f3a6cd6a32b224e038
hentschellj <hentschellj@gmail.com>
2019-09-30 14:03:59 -0500

    Altered import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './SocialCard/SocialCard';
+import SocialCard from './SocialCard/SocialCard';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SocialCard />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
