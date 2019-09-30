e0548c88e461c379fc8f1adfc27f3ddfbd8b5cc9
hentschellj <hentschellj@gmail.com>
2019-09-30 14:04:36 -0500

    Added front and back components for card

---
+++ src/SocialCard/Back/Back.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Back.scss';
+
+class Back extends React.Component {
+    render() {
+        return (
+            <div className="back-side">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <h2>Jack</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Back;
---
+++ src/SocialCard/Front/Front.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Front.scss';
+
+class Front extends React.Component {
+    render() {
+        return (
+            <div className="front-side">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <img src="" />
+                        </div>
+
+                        <div className="col-xs-6">
+                            <h2>Front-End</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Front;
