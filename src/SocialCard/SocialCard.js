5a9706a4c8cf9bc95454aaa007e40662e050e527
hentschellj <hentschellj@gmail.com>
2021-01-05 21:54:03 -0600

    Compiled components, added clickHandle

--- src/SocialCard/SocialCard.js
+++ src/SocialCard/SocialCard.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SocialCard.scss';
-import Front from './Front/Front';
-import Back from './Back/Back';
+import ReactCardFlip from 'react-card-flip';
+import jack from '../assets/jack.png';
 
-function SocialCard() {
+const SocialCard = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped)
+  }
+
   return (
-    <div className="container">
-      <div className="SocialCard">
-        <div className="sides">
-          <Front />
+      <div className="container">
+        <div className="SocialCard">
+          <div className="sides">
+            <ReactCardFlip isFlipped={isFlipped} flipDirection='vertical'>
+              <div className="front-side" onClick={handleClick}>
+                <div className="container front">
+                  <div className="row front">
+                    <div className="col-xs-6 image">
+                      <img src={jack} />
+                    </div>
 
-          <Back />
+                    <div className="col-xs-6 content">
+                      <h2>Jack Hentschell</h2>
+                      <h5>Front-End Web Developer</h5>
+                      <hr />
+                      <h3>314-359-9008</h3>
+                      <h3><a href="mailto:hentschellj@gmail.com">hentschellj@gmail.com</a></h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                
+              <div className="back-side" onClick={handleClick}>
+                <div className="container back">
+                  <div className="row back">
+                    <div className="col-xs-12">
+                      <h2>Life is one big improvisation.</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </ReactCardFlip>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
